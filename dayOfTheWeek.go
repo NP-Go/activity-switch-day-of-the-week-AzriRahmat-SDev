0b20e44fd4be4593aee4589b240b4049d722bb71
Azri Rahmat <azri-rahmat@Azris-MacBook-Pro.local>
2022-04-01 18:21:38 +0800

    adding practice exercise solution

--- dayOfTheWeek.go
+++ dayOfTheWeek.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
 func main() {
 	//Insert code here
+	switch time.Now().Weekday() {
+	case time.Monday, time.Wednesday, time.Friday:
+		fmt.Println("Today is an odd day")
+	case time.Tuesday, time.Thursday:
+		fmt.Println("Today is an odd day")
+	default:
+		fmt.Println("It is a week day")
+	}
 }
